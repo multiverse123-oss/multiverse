a5fcf83dad71502b054b8530c68feaaa8191c7e5
multiverse123-oss <qantumai.dev@gmail.com>
2025-10-26 16:31:47 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";  // ← REMOVE -swc
 import path from "path";
 
 export default defineConfig(() => ({
